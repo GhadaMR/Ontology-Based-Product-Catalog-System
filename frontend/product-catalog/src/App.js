961ed58b9f832f455320322e6d025974f1c4a5e8
GhadaMR <ghadamrassi9@gmail.com>
2025-01-11 16:38:24 +0100

    UI

--- frontend/product-catalog/src/App.js
+++ frontend/product-catalog/src/App.js
@@ -4,8 +4,25 @@ import SparqlQuery from "./SparqlQuery";
 import ProductSearch from "./ProductSearch";
 
 function App() {
+  const styles = {
+    container: {
+      padding: "20px",
+      fontFamily: "'Roboto', sans-serif",
+      maxWidth: "800px",
+      margin: "0 auto",
+      backgroundColor: "#f9f9f9",
+      borderRadius: "8px",
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+    },
+    title: {
+      textAlign: "center",
+      color: "#333",
+      marginBottom: "20px",
+    },
+  };
+  
   return (
-    <div>
+    <div style={styles.container}>
       <h1 style={{ textAlign: "center", marginTop: "20px" }}>Catalogue de Produits Basé sur une Ontologie</h1>
       <ProductSearch />
       <SparqlQuery />
